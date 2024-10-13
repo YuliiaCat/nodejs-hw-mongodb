4db44da99ad8bb3dd0ad73524800d32ceee54394
Yulia Berezniak <kotyuliya24@gmail.com>
2024-10-12 22:57:20 -0400

    fixed

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ export const setupServer = () => {
     });
   });
 
-  app.get('/contacts/:contactId', async (req, res, next) => {
+  app.get('/contacts/:contactId', async (req, res) => {
     const { contactId } = req.params;
     const contact = await getContactById(contactId);
 
