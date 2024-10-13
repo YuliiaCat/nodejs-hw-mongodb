b4a1747ac94a7781125b654d63bc02e141d6e637
Yulia Berezniak <kotyuliya24@gmail.com>
2024-10-12 22:25:04 -0400

    done

--- src/server.js
+++ src/server.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
+import { getAllContacts, getContactById } from './services/contacts.js';
 import { env } from './utils/env.js';
 
 const PORT = Number(env('PORT', '3000'));
 
 export const setupServer = () => {
   const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
   app.use(
     pino({
       transport: {
@@ -15,7 +20,33 @@ export const setupServer = () => {
     }),
   );
 
-  app.use(cors());
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+
+    res.status(200).json({
+      status: 200,
+      message: "Successfully found contacts!",
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      res.status(404).json({
+        message: 'Contact not found',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+  });
 
   app.use('*', (req, res, next) => {
     res.status(404).json({
@@ -26,5 +57,4 @@ export const setupServer = () => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-
 };
