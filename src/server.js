69ba9f250f59188d52c426fa24d5873e7279ef47
Yulia Berezniak <kotyuliya24@gmail.com>
2024-10-12 21:08:35 -0400

    init

--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import cors from 'cors';
+import pino from 'pino-http';
 import { env } from './utils/env.js';
 
 const PORT = Number(env('PORT', '3000'));
 
 export const setupServer = () => {
   const app = express();
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
 
   app.use(cors());
 
