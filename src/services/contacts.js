fbdbd94123d23a7619a14a2f6a5ad39b56bff24c
Yulia Berezniak <kotyuliya24@gmail.com>
2024-10-25 22:09:16 -0400

    upd

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -15,11 +15,11 @@ export const createContact = async (payload) => {
   return contact;
 };
 
-export const updateContact = async (contactId, payload, options = { new: true }) => {
+export const updateContact = async (contactId, payload, options = {}) => {
   const updatedContact = await ContactsCollection.findByIdAndUpdate(
-    contactId,
+    { _id: contactId },
     payload,
-    { ...options, new: true },
+    ...options,
   );
 
   if (!updatedContact || !updatedContact.value) return null;
