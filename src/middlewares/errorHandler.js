ee12dc17f1207a1d0a8845815ae9448cbf8b5527
Yulia Berezniak <kotyuliya24@gmail.com>
2024-10-26 18:38:37 -0400

    errHandler changed

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 import { isHttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res, next) => {
-  if (err instanceof isHttpError) {
+  if (isHttpError(err) === true) {
     return res.status(err.statusCode).json({
       status: err.statusCode,
       message: err.message,
